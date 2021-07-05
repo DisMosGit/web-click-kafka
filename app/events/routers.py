e0160a3d4dfb2741919b6cfbdd3b2b7452f37505
Denis <dissurgu@yandex.ru>
2021-07-05 19:59:18 +0500

    add registrate_click method

--- app/events/routers.py
+++ app/events/routers.py
@@ -1,14 +1,14 @@
 from typing import List
 
-from fastapi import Response, Request, APIRouter, Depends
+from fastapi import APIRouter, Depends
 
 from app.auth.middlewares import get_user_id
-from .models import Click, Word, Following
+from .models import Click
 
 router = APIRouter(prefix="/events")
 
 
-@router.post("/click", response_model=List[Click])
-async def registrate_click(click: Click, user_id: int = Depends(get_user_id)):
-    click.user_id = user_id
-    return click
+@router.post("/click")
+async def registrate_click(click: Click.IN, user_id: int = Depends(get_user_id)):
+    r = await Click.add(click, user_id)
+    return {"ok": True, "count": r}
